551a4af3627840601a318e5f1dd6f9291b356f33
Ling <ling.q.meng@gmail.com>
2017-03-29 14:16:25 -0700

    added correct root folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var config = require('./config/config.js');
 var app = express();
 var port = process.env.PORT || config.PRODUCTION_DEBUG_PORT;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/src'));
 app.listen(port,function (){
   console.log("Motorcycle Knowledge is running at localhost:" + port)
 });
